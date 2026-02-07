6c9f55845a1519e2d5735b13407e2d5362fccee7
Dhiraj Kumar <gdhiraj499@gmail.com>
2026-02-08 01:51:00 +0530

    feat(api): integrate sqlalchemy and user models for persistence

--- app/main.py
+++ app/main.py
@@ -1,7 +1,34 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI,Depends
+from sqlalchemy import create_engine,Column,Integer,String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker,Session
 from prometheus_fastapi_instrumentator import Instrumentator
 
+# Database Configuration
+DATABASE_URL= os.getenv("DATABASE_URL","postgresql://user:password@db:5432/fastapi_db")
+engine = create_engine(DATABASE_URL)
+SessionLocal= sessionmaker(autocommit=False,autoflush=False,bind=engine)
+Base= declarative_base()
+
+# Simple Datbase Model
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer,primary_key=True,index=True)
+    name = Column(String)
+
+# Create tables on startup
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="FastAPI Monitoring Service")
+
+# Dependency to get DB session
+def get_db():
+    db= SessionLocal()
+    try: yield db
+    finally: db.close()
+
+
 
 # New health check endpoint
 @app.get("/health",tags=["Health Check"])
@@ -12,16 +39,19 @@ async def health_check():
 @app.get("/")
 async def root():
     return {
-        "status":"healthy",
-        "version": "2.0.0",
-        "source":"Github Container Registry",
-        "message":"CI/CD Deployment successful!! Pulled latest from GHCR.",
-
+        "message": "Database persistence active!!"
         }
 
-@app.get("/data")
-async def get_data():
-    return {"data":[10,20,30,40,50]}
+@app.post("/users/{name}")
+def create_user(name:str,db:Session=Depends(get_db)):
+    new_user= User(name=name)
+    db.add(new_user)
+    db.commit()
+    return {"status":"User saved","name":name}
+
+@app.get("/users")
+def get_users(db:Session=Depends(get_db)):
+    return db.query(User).all()
 
 # This line captures request latencies,sizes, and counts automatically
 Instrumentator().instrument(app).expose(app)
